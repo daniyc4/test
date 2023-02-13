bedf8718ef606962bdfd1824fb8c6ff18ec8796c
daniyc4 <125304200+daniyc4@users.noreply.github.com>
2023-02-13 20:36:19 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,2 @@
 def run():
-   import requests
-   import base64
-   githubAPIURL = "https://api.github.com/repos/daniyc4/test/contents"
-   githubToken = "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"
-   with open("test.txt", "rb") as f:
-    encodedData = base64.b64encode(f.read())
-    headers = {
-        "Authorization": f'''Bearer {githubToken}''',
-        "Content-type": "application/vnd.github+json"
-    }
-    data = {
-        "message": "My commit message", # Put your commit message here.
-        "content": encodedData.decode("utf-8")
-    }
-
-    r = requests.put(githubAPIURL, headers=headers, json=data)
-    print(r.text) # Printing the response
+   print('Hola Mundo')
