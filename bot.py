3481da22772575aff7436e145fd0934e9edbc647
daniyc4 <125304200+daniyc4@users.noreply.github.com>
2023-02-13 20:58:56 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,14 +4,14 @@ import requests
 # Import the base64 module for encoding a file to base64
 import base64
 
-githubAPIURL = "https://api.github.com/repos/daniyc4/test/contents/test.txt"
+githubAPIURL = "https://api.github.com/repos/daniyc4/test/contents/new-image.jpg"
 # Replace "bracketcounters" with your username, replace "test-repo" with your repository name and replace "new-image.png" with the filename you want to upload from local to GitHub.
 
 # Paste your API token here
-githubToken = "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"
+githubToken = "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"
 
 
-with open("test.txt", "rb") as f:
+with open("my-local-image.jpg", "rb") as f:
     # Encoding "my-local-image.jpg" to base64 format
     encodedData = base64.b64encode(f.read())
 
