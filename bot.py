6b6f042551667757f547b79188d35741a40e2b81
daniyc4 <125304200+daniyc4@users.noreply.github.com>
2023-02-13 20:35:45 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 def run():
    import requests
    import base64
-   githubAPIURL = "https://api.github.com/repos/daniyc4/test/contents/test.txt"
+   githubAPIURL = "https://api.github.com/repos/daniyc4/test/contents"
    githubToken = "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"
    with open("test.txt", "rb") as f:
     encodedData = base64.b64encode(f.read())
